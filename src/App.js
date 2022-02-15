9ce4672c0b189df6921dadc07109b695b5006605
fares-02 <faresbsd@gmail.com>
2022-02-15 13:38:21 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import DrinkDetails from "./Pages/DrinkDetails/DrinkDetails";
+import Drinks from "./Pages/Drinks/Drinks";
+import Errors from "./Pages/Errors/Errors";
+import Home from "./Pages/Home/Home";
 
 function App() {
+  const [text, setText] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar setText={setText} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/drinks" element={<Drinks text={text} />} />
+        <Route path="/drinks/:id_drink" element={<DrinkDetails />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/*" element={<Errors />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
